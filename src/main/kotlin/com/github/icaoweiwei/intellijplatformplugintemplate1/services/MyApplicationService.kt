7c55f215695ddb7d10ac9571ca518fc6cd13ac6d
GitHub Action <action@github.com>
2021-02-19 17:26:51 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/icaoweiwei/intellijplatformplugintemplate1/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.icaoweiwei.intellijplatformplugintemplate1.services
+
+import com.github.icaoweiwei.intellijplatformplugintemplate1.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
